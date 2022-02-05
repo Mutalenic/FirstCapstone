27b37814f287d34a76fcf3df2b2518eab5c6b8dc
Nicholas Mutale <nicomutale@gmail.com>
2022-02-05 11:33:10 +0200

    add javascript update innerhtml

--- main.js
+++
@@ -1,6 +0,0 @@
-const hamburgerMenu = document.querySelector('.toggle-Menu')[0];
-const sidebarMenu = document.querySelector('.aboutMenu')[0];
-
-hamburgerMenu.addEventListener('click',() => {
-    sidebarMenu.classlist.add('active');
-});
